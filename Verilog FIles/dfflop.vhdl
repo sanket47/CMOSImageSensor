7b3cef6a532b9c85627dfaf312c78f2f15676bb7
Sanket Kulkarni <kulkarnisanket47@yahoo.in>
2022-10-08 22:48:52 +0530

    Add files via upload

---
+++ Verilog FIles/dfflop.vhdl
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity D_flip_flop is
+    port (clk,Din : in std_logic;
+             Q: out std_logic;
+             Qnot : out std_logic);
+ end D_flip_flop;
+architecture DFF_arch of D_flip_flop is
+    begin
+        process (clk,Din)
+          begin
+           if(clk’event and clk=’1′) then
+                 Q <= Din;
+                 Qnot <= (not Din);
+               end if;
+        end process;
+end DFF_arch;
